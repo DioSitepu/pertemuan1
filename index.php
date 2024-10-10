551d37f968d9ee408652641e716fe53ab6681e54
DioSitepu <diositepu549@gmail.com>
2024-10-10 00:51:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$nama = "Dio Tri Nanda Sitepu";
+$umur = "19";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>contoh variabel php</title>
+</head>
+<body>
+    <h1>haloo, <?php echo $nama; ?>!</h1>
+    <p>umur anda adalah <php echo $umur;?> tahun,</p>
+</body>
+</html>
